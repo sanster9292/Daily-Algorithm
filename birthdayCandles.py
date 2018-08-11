7a276d72cd865a73c0cad951ca82f91e7105f167
Sanwal Yousaf <sanster9292@users.noreply.github.com>
2018-08-11 01:06:50 -0400

    Changed the name of the file
    Changed the name of the file from buddayCan.py to birthdayCandles.py
    Also, added the link to the HackerRank problem page.

--- birthdayCandles.py
+++ birthdayCandles.py
@@ -1,4 +1,7 @@
-''' PROBLEM STATEMENT
+''' PROBLEM STATEMENT as taken from the following Hacker Rank link
+
+https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
 You are in-charge of the cake for your niece's birthday and have decided the cake will have one
 candle for each year of her total age. When she blows out the candles, she’ll only be able to blow
 out the tallest ones. Your task is to find out how many candles she can successfully blow out.
